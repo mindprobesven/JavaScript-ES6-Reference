e26b5d5f8e9aa42d9163b162a58660bb4dbd0d17
Sven Kohn <sven@mindprobe.io>
2021-06-26 20:23:23 +0200

    add: ES6 Reference

--- ES6 Reference - Sven/Classes/private.js
+++ ES6 Reference - Sven/Classes/private.js
@@ -7,87 +7,79 @@
 // -------------------------------------------------------------------------------------------------------
 
 const Person = (() => {
-  // Here privateProps and getFullNamePrivate() are private because they are in a closure
-  const privateProps = {};
+  // The private properties (privateProps) for each instance of the class are stored in a Map or WeakMap
+  const propsWeakMap = new Map();
 
   // In arrow functions, this retains the value of the enclosing lexical context's this. Therefore, if
   // this arg is passed to call, bind, or apply on invocation of an arrow function it will be ignored.
   // The solution is to use a regular function.
-  function getFullNamePrivate() {
+  function getPrivateFullName() {
     return `${this.first} ${this.last}`;
   }
 
+  function getPrivatePropsWeakMapSize() {
+    return propsWeakMap.size;
+  }
   class PersonClass {
     constructor(first, last, id) {
       this.first = first;
       this.last = last;
 
-      privateProps.privateID = id;
+      const privateProps = { id };
+
+      // We set 'this' as the identifier for each instance of the class that is created and store
+      // the privateProps
+      propsWeakMap.set(this, privateProps);
+    }
+
+    getPrivateID() {
+      // Since 'this' is used as the identifier we can get the privateProps in the Map / WeakMap
+      // for the current class instance calling the getPrivateID() method.
+      return propsWeakMap.get(this).id;
+    }
+
+    setPrivateID(id) {
+      propsWeakMap.set(this, { ...propsWeakMap.get(this), id });
+    }
+
+    getPropsWeakMapSize() {
+      return getPrivatePropsWeakMapSize();
     }
 
     getFullName() {
       // With call(), an object can use a method belonging to another object.
-      // Here, getFullNamePrivate is a method defined outside the class. We
+      // Here, getPrivateFullName is a method defined outside the class. We
       // can use this method inside the class by using call() and passing 'this'
       // which is the PersonClass.
-      return getFullNamePrivate.call(this);
-    }
-
-    getPrivateID() {
-      return privateProps.privateID;
-    }
-
-    setPrivateID(id) {
-      privateProps.privateID = id;
-    }
-
-    get firstName() {
-      return this.first;
-    }
-
-    set firstName(first) {
-      this.first = first;
+      return getPrivateFullName.call(this);
     }
   }
   return PersonClass;
 })();
 
-const sven = new Person('Sven', 'Kohn', '007');
+const sven = new Person('Sven', 'Kohn', '100');
+const barbara = new Person('Barbara', 'Massari', '200');
 
-console.log(sven.first);
+console.log(sven.getPrivateID());       // 100
+console.log(barbara.getPrivateID());    // 200
 
-// Using get and set
-console.log(sven.firstName);
-sven.firstName = 'Thomas';
-console.log(sven.firstName);
+barbara.setPrivateID('9999');
 
-console.log(sven.getFullName());
+console.log(sven.getPrivateID());       // 100
+console.log(barbara.getPrivateID());    // 9999
 
-console.log(sven.getPrivateID());
-sven.setPrivateID('010');
-console.log(sven.getPrivateID());
+console.log(barbara.getPropsWeakMapSize());   // 2 - Because there are two instances of the Person class
 
-console.log(Object.getOwnPropertyNames(sven));
+console.log(sven.getFullName());        // Sven Kohn
+console.log(barbara.getFullName());     // Barbara Massari
+
+// Using this method, private properties and methods are not exposed
+console.log(Object.getOwnPropertyNames(sven));    // [ 'first', 'last' ]
 console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(sven)));
-
-const barbara = new Person('Barbara', 'Massari', '250');
-
-console.log(barbara.first);
-
-// Using get and set
-console.log(barbara.firstName);
-barbara.firstName = 'Valentina';
-console.log(barbara.firstName);
-
-console.log(barbara.getFullName());
-
-console.log(barbara.getPrivateID());
-barbara.setPrivateID('299');
-console.log(barbara.getPrivateID());
-
-console.log(Object.getOwnPropertyNames(barbara));
-console.log(Object.getOwnPropertyNames(Object.getPrototypeOf(barbara)));
-
-console.log(sven.first);
-console.log(sven.getPrivateID());
-console.log(barbara.getPrivateID());
+// [
+//   'constructor',
+//   'getPrivateID',
+//   'setPrivateID',
+//   'getPropsWeakMapSize',
+//   'getFullName'
+// ]
